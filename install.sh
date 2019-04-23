a183380886e7a8a452e1f75d510ff0258ad32cd5
Matthias Wirth <matthias.wirth@gmail.com>
2019-04-23 17:26:46 +0200

    improvements

--- install.sh
+++ install.sh
@@ -2,12 +2,13 @@
 
 sudo cp mirror-json.service /lib/systemd/system
 sudo cp mirror-json.sh /usr/local/bin/
+sudo cp -n default /etc/default/mirror-json
 
 cat 89-mirror-json.conf | ssh $1@$2 "sudo tee /etc/lighttpd/conf-available/89-mirror-json.conf >/dev/null; sudo lighty-enable-mod mirror-json; sudo systemctl restart lighttpd"
 
-sudo sed -i "s/USER=.*/USER=$1/" /usr/local/bin/mirror-json.sh
-sudo sed -i "s/TARGET=.*/TARGET=$2/" /usr/local/bin/mirror-json.sh
+sudo sed -i "s/USER=.*/USER=$1/" /etc/default/mirror-json
+sudo sed -i "s/TARGET=.*/TARGET=$2/" /etc/default/mirror-json
 
-
+sudo systemctl daemon-reload
 sudo systemctl enable mirror-json
 sudo systemctl restart mirror-json
--- mirror-json.sh
+++ mirror-json.sh
@@ -7,15 +7,7 @@ retry=15
 trap "kill 0" SIGINT
 trap "kill -2 0" SIGTERM
 
-SRC=/run/dump1090-fa
-DEST=/run/mirror-json
-
-INTERVAL=10
-HISTORY=80
-
-USER=pi
-TARGET=localhost
-sshopts="-o ControlPath=~/.ssh/cm-%r@%h:%p -o ControlMaster=auto -o ControlPersist=10m -o ConnectTimeout=10 -o ServerAliveInterval=5"
+sshopts="-o ControlPath=~/.ssh/cm-%r@%h:%p -o ControlMaster=auto -o ControlPersist=10m -o ConnectTimeout=5 -o ServerAliveInterval=3"
 
 ssh="ssh $sshopts $USER@$TARGET"
 
@@ -25,18 +17,28 @@ do
 	cat $SRC/receiver.json | sed "s/refresh\" : [0-9]*/refresh\" : ${INTERVAL}000/" | sed "s/history\" : [0-9]*/history\" : $HISTORY/" | bzip2 | $ssh "bunzip2 > $DEST/receiver.json"
 
 	i=0
-
-	while (bzip2 -c $SRC/aircraft.json | $ssh "bunzip2 | tee $DEST/history_$((i%$HISTORY)).json > $DEST/tmp.json && [ -f $DEST/receiver.json ] && mv $DEST/tmp.json $DEST/aircraft.json")
+	while
+		sleep $INTERVAL &
+		pid=$!
+		bzip2 -c $SRC/aircraft.json | $ssh "bunzip2 | tee $DEST/history_$((i%$HISTORY)).json > $DEST/tmp.json && [ -f $DEST/receiver.json ] && mv $DEST/tmp.json $DEST/aircraft.json"
 	do
-		if ! ((i%6))
-		then
-			bzip2 -c $SRC/stats.json | $ssh "bunzip2 > $DEST/stats.json"
-		fi
-		
-		sleep $INTERVAL
+		wait $pid
 		i=$((i+1))
 	done
 	sleep 5
+done &
+
+while sleep 3
+do
+	sleep 57 &
+	pid=$!
+	if ! (bzip2 -c $SRC/stats.json | $ssh "bunzip2 > $DEST/stats-tmp.json && mv $DEST/stats-tmp.json $DEST/stats.json")
+	then
+		echo "Trying to transfer the stats a second time"
+		sleep 10
+		(bzip2 -c $SRC/stats.json | $ssh "bunzip2 > $DEST/stats-tmp.json && mv $DEST/stats-tmp.json $DEST/stats.json")
+	fi
+	wait $pid
 done &
 
 while true
