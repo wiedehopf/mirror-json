f87448bc4219f53de0327bb70624088eea001d11
Matthias Wirth <matthias.wirth@gmail.com>
2019-05-08 16:27:01 +0200

    hopefully make sure it does not hang anymore

--- mirror-json.sh
+++ mirror-json.sh
@@ -11,16 +11,17 @@ sshopts="-o ControlPath=~/.ssh/cm-%r@%h:%p -o ControlMaster=auto -o ControlPersi
 
 ssh="timeout -s9 8 ssh $sshopts $USER@$TARGET"
 
+bzip2='timeout -s9 8 bzip2'
 while true
 do
 	$ssh "sudo mkdir -p $DEST; sudo chown $USER $DEST"
-	cat $SRC/receiver.json | sed "s/refresh\" : [0-9]*/refresh\" : ${INTERVAL}000/" | sed "s/history\" : [0-9]*/history\" : $HISTORY/" | bzip2 | $ssh "bunzip2 > $DEST/receiver.json"
+	timeout -s9 8 cat $SRC/receiver.json | sed "s/refresh\" : [0-9]*/refresh\" : ${INTERVAL}000/" | sed "s/history\" : [0-9]*/history\" : $HISTORY/" | bzip2 | $ssh "bunzip2 > $DEST/receiver.json"
 
 	i=0
 	while
 		sleep $INTERVAL &
 		pid=$!
-		bzip2 -c $SRC/aircraft.json | $ssh "bunzip2 | tee $DEST/history_$((i%$HISTORY)).json > $DEST/tmp.json && [ -f $DEST/receiver.json ] && mv $DEST/tmp.json $DEST/aircraft.json"
+		$bzip2 -c $SRC/aircraft.json | $ssh "bunzip2 | tee $DEST/history_$((i%$HISTORY)).json > $DEST/tmp.json && [ -f $DEST/receiver.json ] && mv $DEST/tmp.json $DEST/aircraft.json"
 	do
 		wait $pid
 		i=$((i+1))
@@ -31,9 +32,9 @@ while sleep 3
 do
 	sleep 57 &
 	pid=$!
-	if ! (bzip2 -c $SRC/stats.json | $ssh "bunzip2 > $DEST/stats-tmp.json && mv $DEST/stats-tmp.json $DEST/stats.json")
+	if ! ($bzip2 -c $SRC/stats.json | $ssh "bunzip2 > $DEST/stats-tmp.json && mv $DEST/stats-tmp.json $DEST/stats.json")
 	then
-		(bzip2 -c $SRC/stats.json | $ssh "bunzip2 > $DEST/stats-tmp.json && mv $DEST/stats-tmp.json $DEST/stats.json")
+		($bzip2 -c $SRC/stats.json | $ssh "bunzip2 > $DEST/stats-tmp.json && mv $DEST/stats-tmp.json $DEST/stats.json")
 	fi
 	wait $pid
 done &
